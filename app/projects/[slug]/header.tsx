ad143f8b5677cc08416d3525274b43dfc42240ae
LENOVO <annisaurohmah@mail.ugm.ac.id>
2024-06-10 06:18:27 +0700

    .

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -122,7 +122,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
               {project.title}
             </h1>
             <div className="mt-6 flex flex-wrap gap-2">
-              {project.types.map((type, index) => (
+              {project.types?.map((type, index) => (
                 <h4
                   key={index}
                   className={`font-bold text-lg px-3 py-1 inline-block rounded ${getTypeClassNames(
