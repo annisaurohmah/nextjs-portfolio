9bc53adb01dc7366220526290f49e588f8aae16c
LENOVO <annisaurohmah@mail.ugm.ac.id>
2024-06-09 12:26:17 +0700

    modify name

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+
+declare module "react" {
+  interface JSXIntrinsicElements {
+    [elemName: string]: any;
+  }
+}
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -13,7 +19,7 @@ export default function Home() {
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
-            <Link
+            <Link<LinkProps>
               key={item.href}
               href={item.href}
               className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
@@ -29,23 +35,23 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Annisa Urohmah
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          A second-year Software Engineering student at Gadjah Mada University.
+          Experienced in Software Requirements Engineering, Software
+          Development, Database Management, Software Design, and Software
+          Testing. Can apply Software Engineering Methodologies in team
+          projects. Interested in mobile application and website development.
+          Experienced in collaborating with multidisciplinary teams to deliver
+          software solutions. Passionate about continuous learning and keeping
+          up with technology. Actively seek opportunities to apply expertise to
+          drive impactful results and contribute to innovative projects.
         </h2>
       </div>
     </div>
   );
-
 }
