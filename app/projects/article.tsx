ee66374adbf5db39d057dc8f4df4e3d9f3756f5e
LENOVO <annisaurohmah@mail.ugm.ac.id>
2024-06-10 07:39:21 +0700

    .

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -7,7 +7,31 @@ type Props = {
 	views: number;
 };
 
+const getTypeClassNames = (types: string[]): string => {
+	const typeClassMap: Record<string, string> = {
+		"Web UI/UX": "bg-blue-500 text-white",
+		"Mobile UI/UX": "bg-green-500 text-white",
+		Frontend: "bg-yellow-500 text-white",
+		Backend: "bg-red-500 text-white",
+		Mobile: "bg-purple-500 text-white",
+	};
+
+	return types
+		.map((type) => {
+			if (type in typeClassMap) {
+				return typeClassMap[type];
+			}
+			return ""; // Or you can skip default class if type is not found
+		})
+		.join(" ");
+};
+
 export const Article: React.FC<Props> = ({ project, views }) => {
+
+	const featuredWithType = {
+		...project,
+		types: project.types?.map((type: string) => type) || [],
+	};
 	return (
 		<Link href={`/projects/${project.slug}`}>
 			<article className="p-4 md:p-8">
@@ -31,6 +55,30 @@ export const Article: React.FC<Props> = ({ project, views }) => {
 				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
 					{project.title}
 				</h2>
+				<div className="mt-4 flex flex-wrap gap-2">
+                  {featuredWithType.types?.map((type, index) => (
+                    <h4
+                      key={index}
+                      className={`font-bold text-md px-3 py-1 inline-block rounded ${getTypeClassNames(
+                        [type]
+                      )}`}
+                    >
+                      {type}
+                    </h4>
+                  ))}
+                </div>
+				<div className="mt-4 flex flex-wrap gap-2">
+                  {featuredWithType.types?.map((type, index) => (
+                    <h4
+                      key={index}
+                      className={`font-bold text-md px-3 py-1 inline-block rounded ${getTypeClassNames(
+                        [type]
+                      )}`}
+                    >
+                      {type}
+                    </h4>
+                  ))}
+                </div>
 				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
 					{project.description}
 				</p>
