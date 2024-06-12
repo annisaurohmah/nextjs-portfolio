b79fb71c49585bec2e8631502d827450b574cd71
LENOVO <annisaurohmah@mail.ugm.ac.id>
2024-06-12 07:27:11 +0700

    try

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import { FaHtml5, FaCss3Alt, FaJs, FaBootstrap, FaReact, FaNodeJs, FaPython, FaPhp, FaJava } from "react-icons/fa"; // Importing icons
-import { SiTailwindcss, SiNextdotjs, SiMysql, SiTypescript, SiPostgresql, SiMongodb, SiDjango, SiLaravel, SiFlutter, SiSwift, SiKotlin } from 'react-icons/si'; // Importing icons
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaBootstrap,
+  FaReact,
+  FaNodeJs,
+  FaPython,
+  FaGit,
+  FaFigma,
+  FaPhp,
+  FaJava,
+} from "react-icons/fa"; // Importing icons
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiMysql,
+  SiTypescript,
+  SiFirebase,
+  SiPostgresql,
+  SiMongodb,
+  SiDjango,
+  SiLaravel,
+  SiFlutter,
+  SiSwift,
+  SiKotlin,
+} from "react-icons/si"; // Importing icons
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -10,24 +35,37 @@ const navigation = [
 ];
 
 const technologies = [
-  { name: 'HTML5', icon: <FaHtml5 className="text-4xl text-red-500" /> },
-  { name: 'CSS3', icon: <FaCss3Alt className="text-4xl text-blue-500" /> },
-  { name: 'JavaScript', icon: <FaJs className="text-4xl text-yellow-500" /> },
-  { name: 'Bootstrap', icon: <FaBootstrap className="text-4xl text-purple-500" /> },
-  { name: 'Tailwind CSS', icon: <SiTailwindcss className="text-4xl text-blue-400" /> },
-  { name: 'Kotlin', icon: <SiKotlin className="text-4xl text-purple-700" /> },
-  { name: 'Python', icon: <FaPython className="text-4xl text-blue-500" /> },
-  { name: 'Laravel', icon: <SiLaravel className="text-4xl text-red-500" /> },
-  { name: 'Next.js', icon: <SiNextdotjs className="text-4xl text-black" /> },
-  { name: 'MySQL', icon: <SiMysql className="text-4xl text-blue-500" /> },
-  { name: 'PHP', icon: <FaPhp className="text-4xl text-indigo-500" /> },
-  { name: 'Java', icon: <FaJava className="text-4xl text-red-600" /> },
-  { name: 'TypeScript', icon: <SiTypescript className="text-4xl text-blue-400" /> },
+  { name: "HTML5", icon: <FaHtml5 className="text-4xl text-red-500" /> },
+  { name: "CSS3", icon: <FaCss3Alt className="text-4xl text-blue-500" /> },
+  { name: "JavaScript", icon: <FaJs className="text-4xl text-yellow-500" /> },
+  {
+    name: "Bootstrap",
+    icon: <FaBootstrap className="text-4xl text-purple-500" />,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <SiTailwindcss className="text-4xl text-blue-400" />,
+  },
+  { name: "Kotlin", icon: <SiKotlin className="text-4xl text-purple-700" /> },
+  { name: "Python", icon: <FaPython className="text-4xl text-blue-500" /> },
+  { name: "Laravel", icon: <SiLaravel className="text-4xl text-red-500" /> },
+  { name: "Next.js", icon: <SiNextdotjs className="text-4xl text-white" /> },
+  { name: "MySQL", icon: <SiMysql className="text-4xl text-blue-500" /> },
+  { name: "PHP", icon: <FaPhp className="text-4xl text-indigo-500" /> },
+  { name: "Java", icon: <FaJava className="text-4xl text-red-600" /> },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript className="text-4xl text-blue-400" />,
+  },
+  { name: "Figma", icon: <FaFigma className="text-4xl text-blue-500" /> },
+  { name: "Git", icon: <FaGit className="text-4xl text-white" /> },
+  { name: "Firebase", icon: <SiFirebase className="text-4xl text-yellow-600"/>}
+
 ];
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+    <div className="flex flex-col items-center justify-center w-screen overflow-y-auto overflow-x-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
@@ -57,6 +95,16 @@ export default function Home() {
         </h2>
       </div>
 
+      <div className="w-full h-px bg-zinc-800" />
+
+
+      <div className="my-16 text-center animate-fade-in mx-16">
+        <h2 className="text-lg text-zinc-500 mb-8">About</h2>
+        <div className="mt-2 px-24 text-zinc-400">
+        A second-year Software Engineering student at Universitas Gadjah Mada. Experienced in Software Requirements Engineering, Software Development, Database Management, Software Design, and Software Testing. Can apply Software Engineering Methodologies in team projects. Interested in mobile application and website development. Experienced in collaborating with multidisciplinary teams to deliver software solutions. Passionate about continuous learning and keeping up with technology. Actively seek opportunities to apply expertise to drive impactful results and contribute to innovative projects.
+        </div>
+      </div>
+
       <div className="my-16 text-center animate-fade-in mx-16">
         <h2 className="text-lg text-zinc-500 mb-8">Technologies I Use</h2>
         <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6">
@@ -69,8 +117,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-    
-    
-
   );
 }
